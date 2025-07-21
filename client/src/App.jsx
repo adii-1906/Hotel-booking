bf8503ab868cec43b9685a726acf32f2868384ea
Aditi Priya <aditipriya460@gmail.com>
2025-07-21 16:30:06 +0530

    Completed Home Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Navbar } from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home';
+import Footer from './components/Footer';
 
 const App = () => {
 
@@ -12,11 +13,14 @@ const App = () => {
       {
         !isOwnerPath && <Navbar/>
       }
+
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home />}/>
         </Routes>
       </div>
+
+      <Footer />
     </div>
   )
 }
